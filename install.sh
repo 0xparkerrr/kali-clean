c38a02e6e5728a7301b7f3035417fbc45186b6b1
Jazz <104627239+jazzpizazz@users.noreply.github.com>
2024-09-03 19:32:58 +0200

    Alacritty can just be apt install-ed

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ get_latest_github_release_file() {
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt-get install -y wget curl git thunar xclip
+sudo apt-get install -y wget curl git thunar xclip alacritty
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson 
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
@@ -27,13 +27,6 @@ wget "$nerd_font_url"
 
 sudo unzip JetBrainsMono.zip -d /usr/local/share/fonts/
 sudo fc-cache -fv
-
-echo "[i] Finding latest version of alacritty..."
-alacritty_url=$(get_latest_github_release_file "alacritty/alacritty" "alacritty.bash")
-echo "Downloading alacritty from: $alacritty_url"
-wget "$alacritty_url"
-sudo bash ./alacritty.bash
-sudo apt install -f
 
 git clone https://github.com/i3/i3 i3
 cd i3 && mkdir -p build && cd build && meson ..
